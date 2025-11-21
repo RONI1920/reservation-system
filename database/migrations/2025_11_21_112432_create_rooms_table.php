8abb69178ea7053546092cb337e0ab5bcbe76c44
Roni Hidayat <ronihidayat66.99@gmail.com>
2025-11-21 22:51:15 +0700

    feat: setup Room Model and migration for room table

--- database/migrations/2025_11_21_112432_create_rooms_table.php
+++ database/migrations/2025_11_21_112432_create_rooms_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
 
             $table->string('name')->unique();
             $table->unsignedSmallInteger('capacity'); //kapasitas ruang
